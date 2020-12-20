ff4bd62865b98d8d5b869a3ad85db7a3bc673962
LMW940423 <tnctis21@naver.com>
2020-12-20 12:38:53 +0900

    인트로-서브(버튼화면)-메인(리스트화면)순 연결 구현 + 홈버튼은 서브로 향함

---
+++ BottomAppbar6/app/src/main/java/com/android/bottomappbar6/SubActivity.java
@@ -0,0 +1,74 @@
+package com.android.bottomappbar6;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SubActivity extends AppCompatActivity {
+    Button insertBtn = null;
+    Button updateBtn = null;
+    Button deleteBtn = null;
+    Button selectBtn = null;
+
+    Intent intent = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        insertBtn = findViewById(R.id.btn_Insert);
+        updateBtn = findViewById(R.id.btn_Update);
+        deleteBtn = findViewById(R.id.btn_Delete);
+        selectBtn = findViewById(R.id.btn_Select);
+
+        insertBtn.setOnClickListener(onClickListener);
+        updateBtn.setOnClickListener(onClickListener);
+        deleteBtn.setOnClickListener(onClickListener);
+        selectBtn.setOnClickListener(onClickListener);
+
+        insertBtn.startAnimation(  AnimationUtils.loadAnimation( SubActivity.this,  R.anim.fade_in ) );
+        updateBtn.startAnimation(  AnimationUtils.loadAnimation( SubActivity.this,  R.anim.fade_in ) );
+        deleteBtn.startAnimation(  AnimationUtils.loadAnimation( SubActivity.this,  R.anim.fade_in ) );
+        selectBtn.startAnimation(  AnimationUtils.loadAnimation( SubActivity.this,  R.anim.fade_in ) );
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            switch (v.getId()){
+                case R.id.btn_Insert:
+                    Toast.makeText(SubActivity.this, "입력화면으로 이동합니다.", Toast.LENGTH_LONG).show();
+
+                    intent = new Intent(SubActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.btn_Update:
+                    Toast.makeText(SubActivity.this, "수정하고자 하는 행을 짧게 클릭하시면 수정화면으로 이동합니다.", Toast.LENGTH_LONG).show();
+
+                    intent = new Intent(SubActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.btn_Delete:
+                    Toast.makeText(SubActivity.this, "삭제하고자 하는 행을 길게 클릭하시면 삭제화면으로 이동합니다.", Toast.LENGTH_LONG).show();
+
+                    intent = new Intent(SubActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    break;
+                case R.id.btn_Select:
+                    Toast.makeText(SubActivity.this, "검색화면으로 이동합니다.", Toast.LENGTH_LONG).show();
+
+                    intent = new Intent(SubActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+
+        }
+    };
+}
